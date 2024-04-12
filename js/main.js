7df9c44229b0ad45eab9abea36f404756704085a
annahico <annahico@gmail.com>
2024-04-12 19:02:08 +0200

    terminando logica formulario

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const formulario = document.getElementById("formulario");
 const nombreInput = document.getElementById("nombre");
-const email = document.getElementById("email");
+const emailInput = document.getElementById("email");
 
 formulario.addEventListener("submit", () => {
-    console.log("Formulario");
+    if (nombreInput.value === "" || emailInput.value === "") {
+        alert("Por favor, complete todos los campos");
+    } else {
+        alert("Formulario enviado correctamente"); //toda la logica para enviar el formulario
+    }
 });
