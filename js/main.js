e58ead93259fd39afc3e413911e2853e274e21e9
annahico <annahico@gmail.com>
2024-04-12 18:54:47 +0200

    probando que funciona el add event

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const formulario = document.getElementById("formulario");
+const nombreInput = document.getElementById("nombre");
+const email = document.getElementById("email");
+
+formulario.addEventListener("submit", () => {
+    console.log("Formulario");
+});
