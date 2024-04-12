d3e557dbd55166d67babdfb2effb8bbebfb2606c
annahico <annahico@gmail.com>
2024-04-12 19:55:52 +0200

    casi terminando ejercicio 11

---
+++ js/ejercicio-11.js
@@ -0,0 +1,36 @@
+const lista = document.getElementById("lista");
+const tareaInput = document.getElementById("tareaInput");
+const botonInput = document.getElementById("agregarTarea");
+
+const agregarTarea = (event) => {
+    if (tareaInput.value.trim() !== "") {
+        const nuevaTarea = document.createElement("li"); //agregar tareas en lista
+        nuevaTarea.textContent = tareaInput.value.trim();
+        lista.appendChild(nuevaTarea);
+        tareaInput.value = ""; //borrar en el cuadrado el texto
+    }
+};
+
+// Add event listener to the button element
+botonInput.addEventListener("click", agregarTarea);
+
+tareaInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        agregarTarea();
+    }
+});
+
+
+
+
+
+
+// agregarTarea.addEventListener("click", () => {
+//     if (tareaInput.value.trim() !== "") {
+//         // console.log("tarea");
+//         // console.log(tareaInput.value.trim().length);
+//         const nuevaTarea = document.createElement("li"); //agregar tareas en lista
+//         nuevaTarea.textContent = tareaInput.value.trim();
+//         lista.appendChild(nuevaTarea);
+//         tareaInput.value = ""; //borrar en el cuadrado el texto
+//     }
